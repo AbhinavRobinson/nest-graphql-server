1ca2ad6e5fafc8950838f8817515dc2c552f9577
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-10-15 21:05:16 +0530

    (feat): add doc

--- src/schema.ts
+++ src/schema.ts
@@ -1,11 +1,16 @@
 import { gql } from 'apollo-server'
 
+/**
+ * Type definitions for the GraphQL schema.
+ */
 const typeDefs = gql`
   type Query {
     hello: String
   }
 `
-
+/**
+ * Resolvers for the GraphQL schema.
+ */
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import { ApolloServer } from 'apollo-server'
 import { resolvers, typeDefs } from './schema'
 
+/**
+ * @description Creates an Apollo Server instance
+ * @returns {ApolloServer} Server instance
+ */
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 })
 
+/**
+ * @description Starts the server
+ */
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`)
 })
